56fa38f8f7892538c0f628e079c70da1cab7b26a
Kondwani Hara <kondwa@gmail.com>
2020-10-20 22:52:45 +0200

    Created some fragments.

---
+++ FragmentExample/app/src/main/java/com/example/android/fragmentexample/SongFragment.java
@@ -0,0 +1,33 @@
+package com.example.android.fragmentexample;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+public class SongFragment extends Fragment {
+
+
+    public SongFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        final View rootView = inflater.inflate(R.layout.fragment_song, container, false);
+        final RatingBar ratingBar = rootView.findViewById(R.id.rating_bar);
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                String r = String.valueOf(v);
+                Toast.makeText(getContext(),"Rating: "+r,Toast.LENGTH_SHORT).show();
+            }
+        });
+        return rootView;
+    }
+}
